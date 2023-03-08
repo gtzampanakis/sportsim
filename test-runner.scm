be32d38064e2810f31f8610a51d6e1eaef030407
Giorgos Tzampanakis <g_tzampanakis@wargaming.net>
2023-03-08 11:57:59 +0200

    WIP

--- test-runner.scm
+++ test-runner.scm
@@ -9,19 +9,7 @@
   ;test-gen-rand-perm))
 
 (define (raise-failed-test obj)
-  (raise-exception 'exception-failed-test #:continuable? #f))
-
-(define test-fns
-  (lambda (name . args)
-    (apply
-      (case name
-        ((assert-eqv)
-          (lambda (a b)
-            (if (not (eqv? a b)) (raise-failed-test (list a b)))))
-        ((assert-equal)
-          (lambda (a b)
-            (if (not (equal? a b)) (raise-failed-test (list a b))))))
-      args)))
+  (raise-exception (cons 'exception-failed-test obj)))
 
 (define (display-run-tests-results outcomes)
   (let loop (
@@ -41,16 +29,43 @@
       (let* (
           (outcome (car outcomes))
           (outcome-type (vector-ref outcome 0))
-          (st (vector-ref outcome 1)))
-        (when (not (null? st))
-          (newline)
-          (display-backtrace st (current-error-port)))
+          (key (vector-ref outcome 1))
+          (args (vector-ref outcome 2))
+          (st (vector-ref outcome 3)))
+        (when (not (null? key))
+          (let ((p (current-error-port)))
+            (newline p)
+            (display "========= START FAILED TEST ==========" p)
+            (newline p)
+            (display key p)
+            (newline p)
+            (display args p)
+            (newline p)
+            (newline p)
+            (display-backtrace st p)
+            (display "========= END FAILED TEST ==========" p)
+            (newline p)
+            (newline p)))
         (loop
           (cdr outcomes)
           (+ n-all 1)
           (+ n-success (if (equal? outcome-type 'success) 1 0))
           (+ n-failure (if (equal? outcome-type 'failure) 1 0))
           (+ n-error (if (equal? outcome-type 'error) 1 0)))))))
+
+(define test-fns
+  (lambda (name . args)
+    (apply
+      (case name
+        ((assert-eqv)
+          (lambda (a b)
+            (if (not (eqv? a b))
+              (raise-failed-test (list name a b)))))
+        ((assert-equal)
+          (lambda (a b)
+            (if (not (equal? a b))
+              (raise-failed-test (list name a b))))))
+      args)))
 
 (define (run-tests)
   (let loop ((test-suite test-suite) (outcomes '()))
@@ -63,7 +78,7 @@
               (call-with-error-handling
                 (lambda ()
                   (test-suite-fn test-fns)
-                  (vector 'success '()))
+                  (vector 'success '() '() '()))
                 #:on-error
                 (lambda (key . args)
                   (set! st (make-stack #t)))
@@ -76,6 +91,8 @@
                         (equal? (car args) 'exception-failed-test))
                       'failure
                       'error)
+                    key
+                    args
                     st))))))
         (loop
           (cdr test-suite)
